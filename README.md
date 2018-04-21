7c62b2379c6375b1f92bd4df88887e0368c4631a
kiosk <letssudormrf@gmail.com>
2018-04-22 00:23:05 +0800

    update Dockerfile

--- README.md
+++ README.md
@@ -13,5 +13,5 @@ Keep the Docker container running automatically after starting, add **--restart 
 
 Outbound enable the socks5 proxy
 
-    docker run --restart always -d -p 80:8080/tcp -p 443:8443/udp -e CONFIG='{"log":{"access":"$ACCESS_LOG","error":"$ERROR_LOG","loglevel":"$LOG_LEVEL"},"inbound":{"port":8880,"protocol":"vmess","settings":{"clients":[{"id":"$UUID","level":1,"alterId":64}]},"streamSettings":{"network":"ws","wsSettings":{"path":"$WSPATH"}}},"outbound":{"protocol":"socks","settings":{"servers":[{"address":"$PROXYIP","port":$PROXYPORT}]}},"outboundDetour":[{"protocol":"blackhole","settings":{},"tag":"blocked"}],"routing":{"strategy":"rules","settings":{"rules":[{"type":"field","ip":["0.0.0.0/8","10.0.0.0/8","100.64.0.0/10","127.0.0.0/8","169.254.0.0/16","172.16.0.0/12","192.0.0.0/24","192.0.2.0/24","192.168.0.0/16","198.18.0.0/15","198.51.100.0/24","203.0.113.0/24","::1/128","fc00::/7","fe80::/10"],"outboundTag":"blocked"}]}}}' -e PROXYIP="socks5-proxyip" -e PROXYPORT="1080" --name v2ray-docker letssudormrf/v2ray-docker
+    docker run --restart always -d -p 80:8080/tcp -p 443:8443/udp -e CONFIG="ON" -e PROXYIP="127.0.0.1" -e PROXYPORT="1080" --name v2ray-docker letssudormrf/v2ray-docker
 
--- entrypoint.sh
+++ entrypoint.sh
@@ -29,13 +29,25 @@ if [ "$CERT" != "$KEY" ]; then
     echo -e "$KEY"  > /tmp/key.pem
 fi
 
+# [SOCKS5PROXY:vars]
+# For using SOCKS5PROXY, set env (e.g. PROXY=ON)
+export PROXY=${PROXY:-""}
+export PROXYIP=${PROXYIP:-"127.0.0.1"}
+export PROXYPORT=${PROXYPORT:-"1080"}
+
+if [ -n "$PROXY" ]; then
+    export outbound=${outbound:-'"outbound":{"protocol":"socks","settings":{"servers":[{"address":"$PROXYIP","port":$PROXYPORT}]}}'}
+else
+    export outbound=${outbound:-'"outbound":{"protocol":"freedom","settings":{}}'}
+fi
+
 # [CONFIG:vars]
 export CONFIG=${CONFIG:-""}
 
 # Mode Selector
 case "$MODE" in
 ws)
-    CONFIG=${CONFIG:-'{"log":{"access":"$ACCESS_LOG","error":"$ERROR_LOG","loglevel":"$LOG_LEVEL"},"inbound":{"port":8880,"protocol":"vmess","settings":{"clients":[{"id":"$UUID","level":1,"alterId":64}]},"streamSettings":{"network":"ws","wsSettings":{"path":"$WSPATH"}}},"outbound":{"protocol":"freedom","settings":{}},"outboundDetour":[{"protocol":"blackhole","settings":{},"tag":"blocked"}],"routing":{"strategy":"rules","settings":{"rules":[{"type":"field","ip":["0.0.0.0/8","10.0.0.0/8","100.64.0.0/10","127.0.0.0/8","169.254.0.0/16","172.16.0.0/12","192.0.0.0/24","192.0.2.0/24","192.168.0.0/16","198.18.0.0/15","198.51.100.0/24","203.0.113.0/24","::1/128","fc00::/7","fe80::/10"],"outboundTag":"blocked"}]}}}'}
+    CONFIG=${CONFIG:-'{"log":{"access":"$ACCESS_LOG","error":"$ERROR_LOG","loglevel":"$LOG_LEVEL"},"inbound":{"port":8880,"protocol":"vmess","settings":{"clients":[{"id":"$UUID","level":1,"alterId":64}]},"streamSettings":{"network":"ws","wsSettings":{"path":"$WSPATH"}}},$outbound,"outboundDetour":[{"protocol":"blackhole","settings":{},"tag":"blocked"}],"routing":{"strategy":"rules","settings":{"rules":[{"type":"field","ip":["0.0.0.0/8","10.0.0.0/8","100.64.0.0/10","127.0.0.0/8","169.254.0.0/16","172.16.0.0/12","192.0.0.0/24","192.0.2.0/24","192.168.0.0/16","198.18.0.0/15","198.51.100.0/24","203.0.113.0/24","::1/128","fc00::/7","fe80::/10"],"outboundTag":"blocked"}]}}}'}
 
     if [ ! -f /tmp/cert.pem ]; then
         openssl req -subj '/CN=www.example.com/O=Internet Corporation for Assigned Names and Numbers/C=US/OU=Technology' -new -x509 -sha256 -days 3650 -nodes -out /tmp/cert.pem -keyout /tmp/key.pem
@@ -52,12 +64,12 @@ ws)
     nginx -g "daemon on;"
     ;;
 tcp)
-    CONFIG=${CONFIG:-'{"log":{"access":"$ACCESS_LOG","error":"$ERROR_LOG","loglevel":"$LOG_LEVEL"},"inbound":{"port":8880,"protocol":"vmess","settings":{"clients":[{"id":"$UUID","level":1,"alterId":64}]},"streamSettings":{"network":"tcp","tlsSettings":{"certificates":[{"certificateFile":"/tmp/cert.pem","keyFile":"/tmp/key.pem"}]},"security":"tls"}},"outbound":{"protocol":"freedom","settings":{}},"outboundDetour":[{"protocol":"blackhole","settings":{},"tag":"blocked"}],"routing":{"strategy":"rules","settings":{"rules":[{"type":"field","ip":["0.0.0.0/8","10.0.0.0/8","100.64.0.0/10","127.0.0.0/8","169.254.0.0/16","172.16.0.0/12","192.0.0.0/24","192.0.2.0/24","192.168.0.0/16","198.18.0.0/15","198.51.100.0/24","203.0.113.0/24","::1/128","fc00::/7","fe80::/10"],"outboundTag":"blocked"}]}}}'}
+    CONFIG=${CONFIG:-'{"log":{"access":"$ACCESS_LOG","error":"$ERROR_LOG","loglevel":"$LOG_LEVEL"},"inbound":{"port":8880,"protocol":"vmess","settings":{"clients":[{"id":"$UUID","level":1,"alterId":64}]},"streamSettings":{"network":"tcp","tlsSettings":{"certificates":[{"certificateFile":"/tmp/cert.pem","keyFile":"/tmp/key.pem"}]},"security":"tls"}},$outbound,"outboundDetour":[{"protocol":"blackhole","settings":{},"tag":"blocked"}],"routing":{"strategy":"rules","settings":{"rules":[{"type":"field","ip":["0.0.0.0/8","10.0.0.0/8","100.64.0.0/10","127.0.0.0/8","169.254.0.0/16","172.16.0.0/12","192.0.0.0/24","192.0.2.0/24","192.168.0.0/16","198.18.0.0/15","198.51.100.0/24","203.0.113.0/24","::1/128","fc00::/7","fe80::/10"],"outboundTag":"blocked"}]}}}'}
     ;;
 *)
     ;;
 esac
 
-echo -e "$CONFIG" | envsubst > /tmp/config.json
+echo -e "$CONFIG" | envsubst | envsubst > /tmp/config.json
 
 v2ray -config /tmp/config.json
