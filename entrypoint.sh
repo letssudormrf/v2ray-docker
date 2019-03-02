d799d520901636dfa588af2979b34e884e60e8af
kiosk <letssudormrf@gmail.com>
2019-03-03 01:19:48 +0800

    update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -27,7 +27,7 @@ export WSPATH=${WSPATH:-"/v2/"}
 export DOHPATH=${DOHPATH:-"/dns/"}
 
 # [Let'sencrypt_Domain:vars]
-export DOMAIN=${DOMAIN:-"0.0.0.0"}
+export DOMAIN=${DOMAIN:-""}
 
 # [Failover_Domain:vars]
 export FAILOVER=${FAILOVER:-"www.example.com"}
